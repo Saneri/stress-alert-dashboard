963fc171c21750413b9984c784e143aa5bedf240
Jukka-Pekka Seppänen <jukka-pekka.seppanen@vincit.fi>
2022-11-06 00:25:16 +0200

    Add bar chart

--- components/Histogram.tsx
+++ components/Histogram.tsx
@@ -1,4 +1,4 @@
-import { Area } from '@ant-design/plots';
+import { Column } from '@ant-design/plots';
 import { FC } from 'react';
 import { StressValues } from '../types/stressValues';
 import { orderBy } from 'lodash';
@@ -6,14 +6,13 @@ import { orderBy } from 'lodash';
 type HistogramProps = {
   values: StressValues[];
   className?: string;
-  channelName?: string;
 };
 
 export const getStressLevel = (v: StressValues): number => {
   return (v.stressValues.stressMessages / v.stressValues.messageCount) * 100;
 };
 
-const Histogram: FC<HistogramProps> = ({ values, className, channelName }) => {
+const Histogram: FC<HistogramProps> = ({ values, className }) => {
   const data = orderBy(
     values.map(({ channelName, timestamp, stressValues }) => ({
       channelName,
@@ -30,11 +29,12 @@ const Histogram: FC<HistogramProps> = ({ values, className, channelName }) => {
 
   const config = {
     data,
+    isStack: true,
     xField: 'timestamp',
     yField: 'Stress level',
     seriesField: 'channelName'
   };
-  return <Area className={className} {...config} />;
+  return <Column className={className} {...config} />;
 };
 
 export default Histogram;
